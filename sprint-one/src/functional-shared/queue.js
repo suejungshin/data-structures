f65d4e42907e136f4556e0603b3024e1fb84085e
Andrii <andrewmalanchuk@gmail.com>
2019-10-23 13:21:25 -0700

    Fixing functional part

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,8 +1,49 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var obj = {};
+  obj.n = 0;
+  obj.storage = {};
+
+  extend(obj, queueMethods);
+
+  return obj;
 };
 
-var queueMethods = {};
+var extend = function(obj, method) {
+  for (var keys in method) {
+    obj[keys] = method[keys];
+  }
+}
+
+var queueMethods = {
+  enqueue: function(string) {
+    var init = 0
+    var keysArr = Object.keys(this.storage);
+    var max = Math.max(keysArr);
+    if (keysArr.length !== 0) {
+      init = max + 1;
+    }
+    this.storage[init] = string;
+    this.n++;
+
+  },
+  dequeue: function() {
+    var deleted = this.storage[0];
+    delete this.storage[0];
+    for (var keys in this.storage) {
+      var copy = Number(keys);
+      copy = copy - 1;
+      this.storage[copy] = this.storage[keys];
+      delete this.storage[keys]
+    }
+    this.n--;
+    if (this.n < 0) this.n = 0;
+    return deleted;
+  },
+  size: function(){
+    return this.n;
+  }
+};
 
 
--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,35 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var obj = {};
+  obj.storage = {};
+  obj.n = 0;
+  extend(obj, stackMethods);
+
+  return obj
 };
 
-var stackMethods = {};
+var extend = function(obj, method) {
+  for (var keys in method) {
+    obj[keys] = method[keys];
+  }
+}
+
+var stackMethods = {
+  push: function(name) {
+    this.storage[this.n] = name;
+    this.n++;
+  },
+  pop: function(){
+    var popped = this.storage[this.n - 1];
+    delete this.storage[this.n - 1];
+    this.n--;
+    if (this.n < 0) this.n = 0;
+    return popped;
+  },
+  size: function(){
+    return this.n;
+  }
+};
 
 
