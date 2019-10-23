edc00a6d4c818906bdde025d2012b183fc3586ef
Andrii <andrewmalanchuk@gmail.com>
2019-10-23 12:14:49 -0700

    Fixing functional part

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -3,16 +3,41 @@ var Queue = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
-
+  var n = 0;
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    var init = 0
+    var keysArr = Object.keys(storage);
+    var max = Math.max(keysArr);
+    if (keysArr.length !== 0) {
+      init = max + 1;
+    }
+    storage[init] = value;
   };
 
   someInstance.dequeue = function() {
+    var deleted = storage[0];
+    delete storage[0];
+    for (var keys in storage) {
+      var copy = Number(keys);
+      copy = copy - 1;
+      storage[copy] = storage[keys];
+      delete storage[keys]
+    }
+    n--;
+    if (n < 0) n = 0;
+    return deleted;
   };
 
   someInstance.size = function() {
+    var count = 0;
+    for (var keys in storage) {
+      if (storage[keys] !== undefined) {
+        count++;
+      }
+    }
+    return count;
   };
 
   return someInstance;
--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -1,17 +1,25 @@
 var Stack = function() {
   var someInstance = {};
-
+  var n = 0;
   // Use an object with numeric keys to store values
   var storage = {};
 
   // Implement the methods below
   someInstance.push = function(value) {
+    someInstance[n] = value;
+    n++;
   };
 
   someInstance.pop = function() {
+    var popped = someInstance[n - 1];
+    delete someInstance[n];
+    n--;
+    if (n < 0) n = 0;
+    return popped;
   };
 
   someInstance.size = function() {
+    return n;
   };
 
   return someInstance;
