a865a97c6f0d897380821be07baa81e839c0364e
Andrii <andrewmalanchuk@gmail.com>
2019-10-23 15:21:10 -0700

    Implementing Prototypal Queue and Stack

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,33 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var obj = Object.create(stackMethods);
+  obj.storage = {};
+  obj.n = 0;
+
+  return obj;
 };
 
-var stackMethods = {};
+var stackMethods = {
+  size: function(){
+    return this.n;
+  },
+  pop: function() {
+    var deleted = this.storage[this.n - 1];
+    delete this.storage[this.n - 1];
+    this.n--;
+    if (this.n < 0) this.n = 0;
+    return deleted;
+  },
+  push: function(name){
+    var init = 0;
+    var keysArr = Object.keys(this.storage);
+    if (keysArr.length !== 0) {
+      init = Math.max(keysArr) + 1;
+    }
+    this.storage[init] = name;
+    this.n++;
+  }
+};
 
 
